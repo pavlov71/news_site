6384c5b177fc86096852776739445c83640cdabc
Yuriy Pavlov <pavlov.pavlusha71@gmail.com>
2024-03-17 09:24:14 +1000

    feat create model, migration and seeder:
    Добавил миграцию, модель, сидер для статьи. А так же перенес в проект файлы верстки их ТЗ

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->realText(120),
+            'description' => $this->faker->realText(),
+            'preview' => $this->faker->realText(50),
+            'thumbnail' => null,
+        ];
+    }
+}
